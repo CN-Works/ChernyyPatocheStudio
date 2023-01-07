2874d7de7c634445f0dd4eda255eb5b0640a53c4
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-01-07 02:52:52 +0100

    1.1 Version
    RageUIx added.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,17 @@ game 'gta5'
 Author "ChernyyOrel#1074"
 lua54 'yes'
 
-client_script "client.lua"
+client_script {
+    -- RageUI
+    'src/RageUI.lua',
+    'src/Menu.lua',
+    'src/MenuController.lua',
+    'src/components/*.lua',
+    'src/elements/*.lua',
+    'src/items/*.lua',
+    --
+    "client.lua",
+}
 
 server_script "server.lua"
 
